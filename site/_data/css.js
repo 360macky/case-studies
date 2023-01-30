106dd8fbbe97456ea43a0c908e0033d89a42a571
Marcelo Arias <hello@marceloarias.com>
2023-01-30 08:27:22 -0500

    Remove Keystone SCSS files

--- site/_data/css.js
+++ site/_data/css.js
@@ -5,11 +5,7 @@ const { compileSassTargets } = require("../utils/compile-scss");
 
 // Add SCSS compilation targets here
 const targets = {
-  main: "site/src/scss/main.scss",
-  "keystone-comments": "site/src/scss/keystone-comments.scss",
-  "keystone-claps": "site/src/scss/keystone-claps.scss",
-  "keystone-login": "site/src/scss/keystone-login.scss",
-  "keystone-bookmarks": "site/src/scss/keystone-bookmarks.scss",
+  main: "site/src/scss/main.scss"
 };
 
 // Create an object with the compiled CSS for each key in targets
